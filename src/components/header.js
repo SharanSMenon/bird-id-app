430e3f1a0a530ffa33a35cc7550fbcf5bd2d6098
sharansajivmenon <sharansajivmenon@gmail.com>
2021-10-29 19:57:49 -0400

    added Beta Label

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
     return (
         <header className="header">
-            <h1 className="display-1">Bird Identifier</h1>
+            <h1 className="display-1">Bird Identifier (BETA)</h1>
         </header>
     );
 }
